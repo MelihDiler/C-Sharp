4847f17dbd930320c42754cbca0a2fc05f9dbe3b
dilermelih <dilermelih@gmail.com>
2025-04-10 18:57:36 +0300

    Proje dosyası ekle.

--- 006-) Byte - Int Degiskenleri/006-) Byte - Int Degiskenleri/Program.cs
+++ 006-) Byte - Int Degiskenleri/006-) Byte - Int Degiskenleri/Program.cs
@@ -21,8 +21,6 @@ namespace _006___Byte___Int_Degiskenleri
             byte BytemaxValue = byte.MaxValue;   // Cikti: 255
             int IntminValue = int.MinValue;      // Cikti: -2147483648
             int IntmaxValue = int.MaxValue;      // Cikti: 2147483647
-            
-
         }
     }
 }
