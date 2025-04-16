1158d292f1eaa5a766e232a5b8172a5770705e59
dilermelih <dilermelih@gmail.com>
2025-04-16 19:48:27 +0300

    first commit

---
+++ 035-) Classlar/035-) Classlar/Araba.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _035___Classlar
+{// Dort cesit erisim belirleyicisi var: public -- private -- protected -- internal
+ // Public: Her yerden erisilir.
+ // Private: Sadece tanımlandığı class içinde erişilebilir.
+    public class Araba
+    {
+        public int kapiSayisi;
+        public string arabaModel;
+        public string arabaRengi;
+
+
+        //  Yapici metotdur. ctor tab tab ile otomatik olusturulur. class ismi ile ayni olmak zorundadir. Nesne olusturulunca ilk olarak otomatik calisir.
+        public Araba(int _kapiSayisi, string _arabaModel, string _arabaRengi)
+        {
+            kapiSayisi = _kapiSayisi;
+            arabaModel = _arabaModel;
+            arabaRengi = _arabaRengi;
+        }
+
+        public void motorCalistir()
+        {
+            Console.WriteLine("Motor Calisiyor...");
+        }
+
+        public void kapilariKilitle()
+        {
+            Console.WriteLine("Kapilar kilitleniyor...");
+        }
+
+
+    }
+}
--- 035-) Classlar/035-) Classlar/Program.cs
+++ 035-) Classlar/035-) Classlar/Program.cs
@@ -11,7 +11,21 @@ namespace _035___Classlar
         static void Main(string[] args)
         {
 
-            // ClassIsmi referans_Isim = new ClassIsmi(); 
+            // ClassIsmi referans_Isim = new ClassIsmi();
+            Araba araba1 = new Araba(4, "Kirmizi", "Renault");//  Bu sekilde parametreli de verebiliriz. Ya da parametresiz verip asagida da tanimlayabilirsin. Parametreli temizdir
+
+            //araba1.kapiSayisi = 4;
+            //araba1.arabaRengi = "Kirmizi";
+            //araba1.arabaModel = "Renault";
+
+            araba1.kapilariKilitle();
+            araba1.motorCalistir();
+
+            Console.WriteLine("Araba kapi sayisi :" + araba1.kapiSayisi);
+            Console.WriteLine("Araba rengi :" + araba1.arabaRengi);
+            Console.WriteLine("Araba modeli :" + araba1.arabaModel);
+            Console.ReadLine();
+
 
 
 
