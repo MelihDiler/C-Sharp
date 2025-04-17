3ef284c81bd1e35843f963233ab384eaa7a5294a
dilermelih <dilermelih@gmail.com>
2025-04-17 15:07:10 +0300

    first commit

---
+++ 036-) Kapsulleme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _036___Kapsulleme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
