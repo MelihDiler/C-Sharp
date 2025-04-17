a7689ecc164b58afec7418239adb1154be30fb97
dilermelih <dilermelih@gmail.com>
2025-04-17 16:00:36 +0300

    fırst commıt

---
+++ 036-) Kapsulleme/Ogrenci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _036___Kapsulleme
+{
+    public class Ogrenci
+    {   //  Amac: isim degiskenimi herhangi biri mudahale edemesin diye. Mudahale etme yontemi de kapsulleme ile ekledik bunlarda setIsim getIsim. Mudahale edilecekse de belli sartlarda edilsin diye. ornegin banka hesabinda bakiye kismi atanacak. Burada 0'dan buyukse sarti koyarak yanlis atamanin onune gecilebilir.
+
+        private string isim;
+
+        //  SET metodunu deger atamak icin, GET metodunu degeri almak icin 
+        public void setIsim(string _isim)                  
+        {
+            isim = _isim;
+        }
+
+        public string getIsim()
+        {
+            return isim;
+        }
+    }
+}
--- 036-) Kapsulleme/Program.cs
+++ 036-) Kapsulleme/Program.cs
@@ -12,7 +12,11 @@ namespace _036___Kapsulleme
         {
 
 
+            Ogrenci ogrenci1 = new Ogrenci();
 
+            ogrenci1.setIsim("Melih");                     //  ogrenci1.isim ile ulasamadik cunku private tanimliydi. Bu sebeple publictanimlanan metot ile ulastik o da setIsim
+            Console.WriteLine(ogrenci1.getIsim());         //  ogrenci1.isim ile ulasamadik cunku private tanimliydi. Bu sebeple publictanimlanan metot ile ulastik o da getIsim
+            Console.ReadLine();
 
 
 
