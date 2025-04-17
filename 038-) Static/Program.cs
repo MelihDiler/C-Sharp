691ba7d4b516a9fbd6659d6a1a191c9b4ea1ee02
dilermelih <dilermelih@gmail.com>
2025-04-17 19:45:19 +0300

    first commit

--- 038-) Static/Program.cs
+++ 038-) Static/Program.cs
@@ -10,6 +10,20 @@ namespace _038___Static
     {
         static void Main(string[] args)
         {
+
+                                                           ////////// STATIC \\\\\\\\\\
+
+
+            //  Eğer bir sınıf static olarak tanımlanmışsa, o sınıfın içindeki tüm üyeler (değişkenler, metotlar, özellikler vb.) de static olmak zorundadır. Nesne ile cagiramazsin.
+            //  Eger bir sinif static degilse metot static ise: Bu sınıftan isteğe bağlı olarak nesne oluşturabilirsin, ama bu metodu nesne oluşturmadan da çağırabilirsin:
+            //  Eger ki sinifta metotta static degilse mecbur nesne ile cagirmak zorundasin.
+
+
+
+
+
+
+
         }
     }
 }
