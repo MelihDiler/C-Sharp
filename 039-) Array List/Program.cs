1de98406b813eb2cbd730440fb585f263abaaae2
dilermelih <dilermelih@gmail.com>
2025-04-18 10:44:41 +0300

    first commit

--- 039-) Array List/Program.cs
+++ 039-) Array List/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;                          //  Bu kutuphane eklenmeden Array List kullanilamaz!!!
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,36 @@ namespace _039___Array_List
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //  using System.Collections.Generic;  kutuphanesi eklenmeden Array List kullanilamaz!!!!!!!!!!!
+
+            ArrayList koleksiyon = new ArrayList();        //  Oncelikle nesne olusturulmali ki ArrayList sinifina erisebilelim.
+
+            koleksiyon.Add("Melih");
+            koleksiyon.Add("Ezgi");
+            koleksiyon.Add("Panduf");
+            koleksiyon.Add("Defne");
+            koleksiyon.Add("Melih");                       //  Ayni degerden birden fazla ekleyebilirsin.
+            koleksiyon.Remove("Melih");                    //  ilk tanimlanan Melih degerini sildi.
+
+            int count = koleksiyon.Count;                  //  Array Listín eleman sayisini dondurur.
+            //koleksiyon.Clear();                          //  Array Listin icini tamamen temizler.
+            koleksiyon.Insert(2, "Simba");                 //  Ikinci indekse Simba degerini ekle demek.
+
+            foreach (var a in koleksiyon)                  //  var kullanmamizin sebebi turu gorup kendi otomatik belirlesin diye.
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine(count);
+            Console.WriteLine(koleksiyon.Contains("Defne"));//  Array Listin icinde var mi kontrolu yapar. Varsa True dondurur.
+            Console.WriteLine(koleksiyon.IndexOf("Panduf"));//  Cikti = 1 cunku ilk Melih degeri silinmisti onceki satirlarda. Eger ki deger yoksa -1 doner.
+            Console.ReadLine();
+
+
+
+
+
+
+
         }
     }
 }
