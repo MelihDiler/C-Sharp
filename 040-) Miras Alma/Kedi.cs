5b7d5816dd19e291046b5a029e5852ed7e868289
dilermelih <dilermelih@gmail.com>
2025-04-18 13:41:40 +0300

    first commit

--- 040-) Miras Alma/Kedi.cs
+++ 040-) Miras Alma/Kedi.cs
@@ -8,8 +8,7 @@ namespace _040___Miras_Alma
 {
     public class Kedi : Hayvan
     {
-
+        //  Bir sinif sadece bir adet sinifi miras alabilir.
         //  Burada hicbir sey tanimlanmadi fakat Hayvan sinifindan miras aldigi icin Hayvan sinifindaki her seye bu siniftan erisilebiliyor.
-
     }
 }
