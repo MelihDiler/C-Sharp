734be7811e668bc41132f7f704a0d5c2814b0011
dilermelih <dilermelih@gmail.com>
2025-04-24 16:38:17 +0300

    first commit

---
+++ 046-) Generic Collections/Musteri.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _046___Generic_Collections
+{
+    public class Musteri
+    {
+        private int id;
+        private string isim;
+        private string soyisim;
+        private string email;
+
+        public Musteri(int _id, string _isim, string _soyisim, string _email)
+        {
+            this.id = _id;
+            this.isim = _isim;
+            this.soyisim = _soyisim;
+            this.email = _email;
+        }
+
+        public override string ToString()                  //  Bu kod icerdeki degerleri string tipinde vermemizi sagladi.
+        {
+            return $"ID: {id}, İsim: {isim}, Soyisim: {soyisim}, Email: {email}";
+        }
+
+
+    }
+}
--- 046-) Generic Collections/Program.cs
+++ 046-) Generic Collections/Program.cs
@@ -12,11 +12,30 @@ namespace _046___Generic_Collections
         static void Main(string[] args)
         {   //  Tip guvenligi vardir. Hangi tipte olusturulduysa sadece o tipte eleman kabul eder.
 
-
-            List<int> rakamlar = new List<int>();
+            List<int> rakamlar = new List<int>();          //  List int tipinde rakamlar adinda degisken olusturup, int tipinde List nesnesini atiyoruz.
 
             rakamlar.Add(0);
-            //  rakamlar.Add("0");           HATA verir. cunku sadece int kabul eder.
+            //  rakamlar.Add("0");           HATA verir. Cunku sadece int kabul eder.
+
+            
+            
+            
+            
+            //-------------------------------------------------------------------------------------------------------------------------
+
+            List<Musteri> musteriler = new List<Musteri>();//  Musteri sinifi turunde List tipinde musteriler adinda degisken tanimliyorruz ve Musteriler turunde List nesnesini buna tiyoruz.
+            musteriler.Add(new Musteri(1, "Melih", "Diler", "dilermelih@gmail.com"));//musteriler degiskeni Musteri turunde tanimlandigi icin icine Musteri sinifi turunde 
+            musteriler.Add(new Musteri(2, "Ezgi", "Diler", "dilerezgi@gmail.com"));  //deger atabiliriz. Bu sebeple degerden ziyade nesne atmamiz gerekir nesne tanimlayarak 
+            musteriler.Add(new Musteri(3, "Panduf", "Diler", "dilerpanduf@gmail.com"));//degerleri atadik. Nesneleri asagida tek tek olusturup atadigimiz degiskenleri de 
+                                                                                       //buraya parantez icine yazabilirdik.
+            foreach (Musteri musteri in musteriler)   //  musteriler generic listesi icinde her gezdigimiz nesne degerini Musteri sinifi tipindeki musteri degiskenine atiyoruz.
+            {
+                Console.WriteLine(musteri);
+            }
+
+            Console.ReadLine();
+
+            
         }
     }
 }
